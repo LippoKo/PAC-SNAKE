a68ab5cb3469f236aff4d25f3b0ed65f3858181a
Luiz Lima <luizlimadevpro@gmail.com>
2022-07-21 17:21:33 +0100

    playable2

--- scripts/enemies.js
+++ scripts/enemies.js
@@ -21,10 +21,10 @@ class Enemies {
 	enemyMove() {
 		this.x += this.vx;
 		this.y += this.vy;
-		if (this.y + this.vy > 600 || this.y + this.vy < 0) {
+		if (this.y + this.vy > 580 || this.y + this.vy < 0) {
 			this.vy *= -1;
 		}
-		if (this.x + this.vx > 600 || this.x + this.vx < 0) {
+		if (this.x + this.vx > 580 || this.x + this.vx < 0) {
 			this.vx *= -1;
 		}
 	}
